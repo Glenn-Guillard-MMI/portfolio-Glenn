31e5c8fc390f3c5182afae81aca2dcb25afc512d
Alzower <glenn.guillard.pros@gmail.com>
2024-08-04 17:43:41 +0200

    install and use gsap

--- components/head-component.vue
+++ components/head-component.vue
@@ -1,9 +1,31 @@
 <template>
   <header>
     <div>
-      <h1>Glenn <br />Guillard</h1>
-      <h2>Développeur web</h2>
+      <h1 class="pres">Glenn <br />Guillard</h1>
+      <h2 class="pres">Développeur web</h2>
     </div>
-    <img src="../static/images/tour-eiffel-2.png" alt="Tour Eiffel" />
+    <img
+      id="eiffelTower"
+      src="../static/images/tour-eiffel-2.png"
+      alt="Tour Eiffel"
+    />
   </header>
 </template>
+<script setup lang="ts">
+const { $gsap } = useNuxtApp();
+
+onMounted(() => {
+  $gsap.from(".pres", {
+    duration: 3.5,
+    opacity: 0,
+    y: 150,
+    ease: "power4.out",
+  });
+
+  $gsap.from("#eiffelTower", {
+    duration: 2,
+    opacity: 0,
+    ease: "bounce.out",
+  });
+});
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,12 @@
 export default defineNuxtConfig({
   app: {
     head: {
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
-    }
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+    },
   },
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  css: [
-    '@/assets/main.css'
-  ]
-})
+  css: ["@/assets/main.css"],
+  modules: ["@hypernym/nuxt-gsap"],
+});
